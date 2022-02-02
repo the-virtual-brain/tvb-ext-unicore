fd0f091e1a5c1b5027db472b626f5e533c8b8d3d
liadomide <lia_domide@yahoo.com>
2022-02-02 12:09:00 +0200

    fix syntax

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
 import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
+    JupyterFrontEnd,
+    JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ISettingRegistry } from '@jupyterlab/settingregistry';
+import {ISettingRegistry} from '@jupyterlab/settingregistry';
 
 /**
  * Initialization data for the tvb-ext-unicore extension.
  */
-const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'tvb-ext-unicore:plugin',
-  autoStart: true,
-  optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension tvb-ext-unicore is activated!');
+const plugin: { activate: (app: JupyterFrontEnd, settingRegistry: (ISettingRegistry | null)) => void; optional: (Token<ISettingRegistry> | ISettingRegistry)[]; id: string; autoStart: boolean } = {
+    id: 'tvb-ext-unicore:plugin',
+    autoStart: true,
+    optional: [ISettingRegistry],
+    activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+        console.log('JupyterLab extension tvb-ext-unicore is activated!');
 
-    if (settingRegistry) {
-      settingRegistry
-        .load(plugin.id)
-        .then(settings => {
-          console.log('tvb-ext-unicore settings loaded:', settings.composite);
-        })
-        .catch(reason => {
-          console.error('Failed to load settings for tvb-ext-unicore.', reason);
-        });
+        if (settingRegistry) {
+            settingRegistry
+                .load(plugin.id)
+                .then(settings => {
+                    console.log('tvb-ext-unicore settings loaded:', settings.composite);
+                })
+                .catch(reason => {
+                    console.error('Failed to load settings for tvb-ext-unicore.', reason);
+                });
+        }
     }
-  }
 };
 
 export default plugin;
