a358fb7b09773f767fd3e4706a8e8e5901bb55cb
davidbacter01 <davidbacter01@gmail.com>
2022-02-08 11:10:56 +0200

    Initial UI + todos

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+// todo: add WidgetTracker and ILayoutRestorer to monitor state
+
+import { PyunicoreWidget, DataType } from "./pyunicoreWidget";
 import { requestAPI } from './handler';
+// todo: create an interface for API response
 
 /**
  * Initialization data for the tvb-ext-unicore extension.
@@ -11,9 +16,64 @@ import { requestAPI } from './handler';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'tvb-ext-unicore:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension tvb-ext-unicore is activated!');
+    console.log('ICommandPalette:', palette);
+    // todo: replace constant with api call (or add method to be called in constructor maybe tbd)
+    const data: DataType = {
+      rows: [
+        {id: "mockid_1", name: "mock_name_1", user: "mock_user_1", time: "mock_time_1"},
+        {id: "mockid_2", name: "mock_name_2", user: "mock_user_2", time: "mock_time_2"},
+        {id: "mockid_3", name: "mock_name_3", user: "mock_user_3", time: "mock_time_3"},
+        {id: "mockid_4", name: "mock_name_4", user: "mock_user_4", time: "mock_time_4"},
+        {id: "mockid_5", name: "mock_name_5", user: "mock_user_5", time: "mock_time_5"},
+      ]
+    }
 
+    const content = new PyunicoreWidget({cols: ["id", "name", "user", "time"]}, data);
+    const widget = new MainAreaWidget({ content });
+    widget.id = 'tvb-ext-unicore';
+    widget.title.label = 'PyUnicore Task Stream';
+    widget.title.closable = true;
+
+    function updateTasks():void {
+      //call api
+      requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+        //set data to table (currently mock data)
+        content.data = {
+          rows: [
+            {id: "updated_id", name: "updated_name", user: "updated_user", time: "mock_time"}
+          ]
+        };
+      })
+      .catch(reason => {
+        console.error(
+          `The tvb-ext-unicore server extension appears to be missing.\n${reason}`
+        );
+      });
+    }
+
+    //add a button to update tasks table
+    const btn = document.createElement("button");
+    btn.innerText = "Update Tasks";
+    btn.onclick = updateTasks;
+    content.node.prepend(btn);
+
+    const command: string = 'tvb-ext-unicore:open';
+    app.commands.addCommand(command, {
+      label: 'PyUnicore Task Stream',
+      execute: () => {
+        if (!widget.isAttached) {
+          app.shell.add(widget, 'main');
+        }
+        app.shell.activateById(widget.id);
+      }
+    })
+
+    palette.addItem({command, category: 'PyUnicore'});
     requestAPI<any>('get_example')
       .then(data => {
         console.log(data);
---
+++ src/pyunicoreWidget.ts
@@ -0,0 +1,112 @@
+import { Widget } from '@lumino/widgets';
+
+export interface TableFormat {
+    cols: Array<string>
+}
+
+export interface DataType {
+    rows: any[]
+}
+export class PyunicoreWidget extends Widget {
+    constructor(format: TableFormat, data: DataType) {
+        // todo: see if there is a better way to use element creation in widgets (maybe a template?)
+        super();
+        this.addClass("tvb-pyunicoreWidget");
+        this.table = document.createElement("table");
+        this.tHead = document.createElement("thead");
+        this.tBody = document.createElement("tbody");
+        this.table.appendChild(this.tHead);
+        this.table.appendChild(this.tBody);
+        this.node.appendChild(this.table);
+        this.tableFormat = format;
+        this._data = data;
+        this.buildTable();
+    }
+
+    /**
+     * Data for table - fixme: currently this handles state and ui sync - there must be a better way to do this
+     */
+    private _data: DataType;
+    public get data() {
+        return this._data;
+    }
+    public set data(data: DataType) {
+        this._data = data;
+        this.buildTBody();
+    }
+
+    /**
+     * The jobs table
+     */
+    readonly table: HTMLTableElement;
+
+    /**
+     * Table header
+     */
+    readonly tHead: HTMLElement;
+
+    /**
+     * Table body
+     */
+    readonly tBody: HTMLElement;
+
+    /**
+     * Table format
+     */
+    readonly tableFormat: TableFormat;
+
+    /**
+     * builder function for table
+     */
+    buildTable(): void {
+        this.tHead.innerHTML = '';
+        this.buildTHead();
+        this.tBody.innerHTML = '';
+        this.buildTBody();
+    }
+
+    buildTHead(): void{
+        const tr = document.createElement("tr");
+        this.tHead.appendChild(tr);
+        this.tableFormat.cols.forEach((colText)=> {
+            let thCol = document.createElement("th");
+            thCol.innerText = colText;
+            tr.appendChild(thCol);
+        })
+        let th = document.createElement("th");
+        th.innerText = "Actions";
+        tr.appendChild(th);
+    }
+
+    buildTBody(): void {
+        console.log("build tbody");
+        this.tBody.innerHTML = "";
+        // function to cancel a job fixme: have it passed down as param for a more dynamic widget
+        const cancelJob = (id: string): void => {
+            console.log("cancelling job");
+            const newData = this.data;
+            // todo: api call to cancel job
+            newData.rows = newData.rows.filter((row)=>row.id!==id);
+            this.data = newData;
+        }
+        this.data.rows.forEach((rowData: any)=>{
+            let tr = document.createElement("tr");
+            let id = rowData["id"]; //fixme: should be dynamic
+            tr.id = id;
+            this.tableFormat.cols.forEach((colName: string) => {
+               let td = document.createElement("td");
+               td.innerText = rowData[colName];
+               tr.appendChild(td);
+            });
+
+            // add button to cancel job
+            const btn = document.createElement("button");
+            btn.innerText = "Cancel Job";
+            btn.onclick = () => cancelJob(id);
+            let td = document.createElement("td");
+            td.appendChild(btn);
+            tr.appendChild(td);
+            this.tBody.appendChild(tr);
+        });
+    }
+}
