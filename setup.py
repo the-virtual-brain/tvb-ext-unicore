be5b44caa5436bec71eb7c7930e3fca2eaf83591
popaula937 <paula.popa@codemart.ro>
2022-03-01 18:56:39 +0200

    WID-27. Prepare github action for releasing extension on pypi.

--- setup.py
+++ setup.py
@@ -57,7 +57,8 @@ setup_args = dict(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
-        "jupyter_server>=1.6,<2"
+        "jupyter_server>=1.6,<2",
+        "pyunicore >= 0.9.15"
     ],
     zip_safe=False,
     include_package_data=True,
