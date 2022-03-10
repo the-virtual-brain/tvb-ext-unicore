9d4c3dc3f51944d581e3eba50b879ed573fe4124
liadomide <lia_domide@yahoo.com>
2022-03-10 17:30:56 +0200

    WID-35 Fix small lint suggestions

--- tvbextunicore/_version.py
+++ tvbextunicore/_version.py
@@ -1,23 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# "TheVirtualBrain - Widgets" package
+#
+# (c) 2022-2023, TVB Widgets Team
+#
+
 import json
 from pathlib import Path
 
 __all__ = ["__version__"]
 
-def _fetchVersion():
-    HERE = Path(__file__).parent.resolve()
 
-    for settings in HERE.rglob("package.json"): 
+def _fetch_version():
+    here = Path(__file__).parent.resolve()
+
+    for settings in here.rglob("package.json"):
         try:
             with settings.open() as f:
                 version = json.load(f)["version"]
                 return (
                     version.replace("-alpha.", "a")
-                    .replace("-beta.", "b")
-                    .replace("-rc.", "rc")
+                        .replace("-beta.", "b")
+                        .replace("-rc.", "rc")
                 )
         except FileNotFoundError:
             pass
 
-    raise FileNotFoundError(f"Could not find package.json under dir {HERE!s}")
+    raise FileNotFoundError(f"Could not find package.json under dir {here!s}")
 
-__version__ = _fetchVersion()
+
+__version__ = _fetch_version()
