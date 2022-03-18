55089226296e6b8837879cbce542399eccce4d6c
davidbacter01 <davidbacter01@gmail.com>
2022-03-18 16:26:43 +0200

    WID-38: Fix issue after rebase

--- src/index.ts
+++ src/index.ts
@@ -50,14 +50,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
   ) => {
     console.log('JupyterLab extension tvb-ext-unicore is activated!');
     let widget: MainAreaWidget<PyunicoreWidget>;
-    let sites: string[] = [];
-    let message = '';
-    try {
-      sites = await requestAPI<any>('sites');
-    } catch (e) {
-      message = 'Could not retrieve sites from HBP!';
-      console.log(e);
-    }
 
     const columns = ['id', 'name', 'owner', 'site', 'status', 'start_time'];
     const command = 'tvbextunicore:open';
@@ -72,7 +64,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
               idField: 'id',
               buttonRenderConditionField: 'is_cancelable'
             },
-            data: { message: message, jobs: [] },
+            data: { message: '', jobs: [] },
             buttonSettings: {
               onClick: cancelJob,
               onClickFieldArgs: ['resource_url'],
