2e977dbedbc0d1d5f4e31382e9b6ae3899bf9428
davidbacter01 <davidbacter01@gmail.com>
2022-03-18 11:02:55 +0200

    fix sonar issue

--- src/__tests__/ModalComponent.test.tsx
+++ src/__tests__/ModalComponent.test.tsx
@@ -1,5 +1,5 @@
 import { ModalWidget, ModalType } from '../components/ModalComponent';
-import {findByText, fireEvent, render} from '@testing-library/react';
+import { findByText, fireEvent, render } from '@testing-library/react';
 import React from 'react';
 
 const mockSetVisible = jest.fn((val: boolean) => {
