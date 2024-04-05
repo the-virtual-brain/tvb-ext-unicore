7608205cd34274d02124106cb4c9a7973ad63e4d
liadomide <lia_domide@yahoo.com>
2024-04-05 23:47:55 +0300

    EBR-40 Test on more Python versions

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Framework :: Jupyter",
         "Framework :: Jupyter :: JupyterLab",
         "Framework :: Jupyter :: JupyterLab :: 3",
